9bc99ac5ff5353fdc0138b4108f9a9f5938ba5e7
Mathias Behrle <mathiasb@behrle.dyndns.org>
2009-01-12 13:22:18 +0100

    Add translation for de_DE Modify descriptions and add german translation

--- __tryton__.py
+++ __tryton__.py
@@ -2,15 +2,16 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name': 'Account Invoice Discount',
-    'name_de_DE': 'Rabattspalte für Rechnung',
+    'name_de_DE': 'Fakturierung Rabatt',
     'version': '0.0.1',
     'author': 'virtual things',
     'email': 'info@virtual-things.biz',
     'website': 'http://www.virtual-things.biz/',
-    'description': '''This module adds a discount column on invoice
+    'description': '''Define discounts for invoice lines
+    - with additional field discount in report invoice
 ''',
-    'description_de_DE': '''Dieses Modul fügt dem Rechnungsformular eine
-    Rabattspalte hinzu
+    'description_de_DE': '''Ermöglicht die Eingabe von Rabatten pro Rechnungszeile
+    - mit zusätzlichem Rabattfeld im Bericht Rechnung
 ''',
     'depends': [
         'account_invoice'
@@ -18,7 +19,7 @@
     'xml': [
         'invoice.xml'
         ],
-#    'translation': [
-#        'de_DE.csv'
-#    ],
+    'translation': [
+        'de_DE.csv'
+    ],
 }
