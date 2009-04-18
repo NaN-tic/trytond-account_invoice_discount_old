4d0805ba0537335547cb1f4f6a85e983aa1a8036
Korbinian Preisler <timitos@virtual-things.biz>
2009-04-18 20:11:18 +0200

    Use super to keep modularity

--- invoice.py
+++ invoice.py
@@ -25,27 +25,17 @@ class InvoiceLine(ModelSQL, ModelView):
         return 0.0
 
     def on_change_with_amount(self, cursor, user, ids, vals, context=None):
-        currency_obj = self.pool.get('currency.currency')
-        if vals.get('type') == 'line':
-            currency = vals.get('_parent_invoice.currency',
-                                vals.get('currency'))
-            if isinstance(currency, (int, long)):
-                currency = currency_obj.browse(cursor, user, currency, \
-                        context=context)
-            discount = Decimal(str(vals.get('quantity') or Decimal('0.0'))) * \
-                         (vals.get('unit_price') or Decimal('0.0')) * \
-                         ((((vals.get('discount') or Decimal('0.0'))* \
-                            Decimal('0.01'))) or Decimal('0.0'))
-            amount = Decimal(str(vals.get('quantity') or '0.0')) * \
-                         (vals.get('unit_price') or Decimal('0.0')) - discount
-            if currency:
-                return currency_obj.round(cursor, user, currency, amount)
-            return amount
-        return Decimal('0.0')
+        if vals.get('type') == 'line' and vals.get('discount'):
+            vals = vals.copy()
+            vals['unit_price'] = (vals.get('unit_price') -
+                vals.get('unit_price') * vals.get('discount') * Decimal('0.01'))
+        return super(InvoiceLine, self).on_change_with_amount(cursor, user,
+                                                ids, vals, context=context)
 
     def get_amount(self, cursor, user, ids, name, arg, context=None):
         currency_obj = self.pool.get('currency.currency')
-        res = {}
+        res = super(InvoiceLine, self).get_amount(cursor, user, ids, name, arg,
+                                                  context=context)
         for line in self.browse(cursor, user, ids, context=context):
             if line.type == 'line':
                 currency = line.invoice and line.invoice.currency \
@@ -54,38 +44,15 @@ class InvoiceLine(ModelSQL, ModelView):
                         Decimal(str(line.quantity)) * line.unit_price - \
                             (Decimal(str(line.quantity)) * line.unit_price *\
                             (line.discount * Decimal('0.01'))))
-            elif line.type == 'subtotal':
-                res[line.id] = Decimal('0.0')
-                for line2 in line.invoice.lines:
-                    if line2.type == 'line':
-                        res[line.id] += currency_obj.round(cursor, user, \
-                            line2.invoice.currency,
-                            Decimal(str(line2.quantity)) * line2.unit_price - \
-                            (Decimal(str(line2.quantity)) * line2.unit_price * \
-                            (line2.discount * Decimal('0.01'))))
-                    elif line2.type == 'subtotal':
-                        if line.id == line2.id:
-                            break
-                        res[line.id] = Decimal('0.0')
-            else:
-                res[line.id] = Decimal('0.0')
         return res
 
     def _credit(self, cursor, user, line, context=None):
         '''
         Return values to credit line.
         '''
-        res = {}
-        for field in ('sequence', 'type', 'quantity', 'unit_price', \
-                'description', 'discount'):
-            res[field] = line[field]
-
-        for field in ('unit', 'product', 'account'):
-            res[field] = line[field].id
-
-        res['taxes'] = []
-        for tax in line.taxes:
-            res['taxes'].append(('add', tax.id))
+        res = super(InvoiceLine, self)._credit(cursor, user, line,
+                                                  context=context)
+        res['discount'] = line['discount']
         return res
 
     def _compute_taxes(self, cursor, user, line, context=None):
