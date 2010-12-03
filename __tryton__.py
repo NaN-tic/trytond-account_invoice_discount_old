9daf9650ec5bed65befdc2c80ff7138c48b7bebd
Udo Spallek <udono@virtual-things.biz>
2010-12-03 19:52:11 +0100

    Increase version number

--- __tryton__.py
+++ __tryton__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Account Invoice Discount',
     'name_de_DE': 'Fakturierung Rabatt',
-    'version': '1.4.1',
+    'version': '1.5.0',
     'author': 'virtual things',
     'email': 'info@virtual-things.biz',
     'website': 'http://www.virtual-things.biz/',
