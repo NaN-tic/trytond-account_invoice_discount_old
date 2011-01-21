a394d1108a0f3a8a5243b896f5b727ed6df82d85
Udo Spallek <udono@virtual-things.biz>
2011-01-21 04:33:53 +0100

    Update for new Transaction.

--- invoice.py
+++ invoice.py
@@ -1,9 +1,11 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
+import copy
+from decimal import Decimal
+
 from trytond.model import ModelView, ModelSQL, fields
 from trytond.pyson import Not, Equal, Eval
-from decimal import Decimal
-import copy
+
 
 class InvoiceLine(ModelSQL, ModelView):
     _name = 'account.invoice.line'
@@ -19,71 +21,65 @@ class InvoiceLine(ModelSQL, ModelView):
             self.amount.on_change_with.extend(['discount'])
         self._reset_columns()
 
-    def default_discount(self, cursor, user, context=None):
+    def default_discount(self):
         return 0.0
 
-    def on_change_with_amount(self, cursor, user, vals, context=None):
+    def on_change_with_amount(self, vals):
         if vals.get('type') == 'line' and vals.get('discount'):
             vals = vals.copy()
             vals['unit_price'] = (vals.get('unit_price') -
                 vals.get('unit_price') * vals.get('discount') * Decimal('0.01'))
-        return super(InvoiceLine, self).on_change_with_amount(cursor, user,
-                                                vals, context=context)
+        return super(InvoiceLine, self).on_change_with_amount(vals)
 
-    def get_amount(self, cursor, user, ids, name, context=None):
+    def get_amount(self, ids, name):
         currency_obj = self.pool.get('currency.currency')
-        res = super(InvoiceLine, self).get_amount(cursor, user, ids, name,
-                                                  context=context)
-        for line in self.browse(cursor, user, ids, context=context):
+        res = super(InvoiceLine, self).get_amount(ids, name)
+        for line in self.browse(ids):
             if line.type == 'line':
                 currency = line.invoice and line.invoice.currency \
                         or line.currency
-                res[line.id] = currency_obj.round(cursor, user, currency, \
+                res[line.id] = currency_obj.round(currency, \
                         Decimal(str(line.quantity)) * line.unit_price - \
                             (Decimal(str(line.quantity)) * line.unit_price *\
                             (line.discount * Decimal('0.01'))))
         return res
 
-    def _credit(self, cursor, user, line, context=None):
+    def _credit(self, line):
         '''
         Return values to credit line.
         '''
-        res = super(InvoiceLine, self)._credit(cursor, user, line,
-                                                  context=context)
+        res = super(InvoiceLine, self)._credit(line)
         res['discount'] = line['discount']
         return res
 
-    def _compute_taxes(self, cursor, user, line, context=None):
+    def _compute_taxes(self, line):
         tax_obj = self.pool.get('account.tax')
         currency_obj = self.pool.get('currency.currency')
         invoice_obj = self.pool.get('account.invoice')
-        if context is None:
-            context = {}
 
-        ctx = context.copy()
-        ctx.update(invoice_obj.get_tax_context(cursor, user, line.invoice,
-            context=context))
+        context = {}
+        context.update(invoice_obj.get_tax_context(line.invoice))
         res = []
         if line.type != 'line':
             return res
         price = line.unit_price - line.unit_price * line.discount / Decimal('100')
         tax_ids = [x.id for x in line.taxes]
-        for tax in tax_obj.compute(cursor, user, tax_ids, price,
-                line.quantity, context=ctx):
-            if line.invoice.type in ('out_invoice', 'in_invoice'):
-                base_code_id = tax['tax'].invoice_base_code.id
-                amount = tax['base'] * tax['tax'].invoice_base_sign
-            else:
-                base_code_id = tax['tax'].credit_note_base_code.id
-                amount = tax['base'] * tax['tax'].credit_note_base_sign
-            if base_code_id:
-                amount = currency_obj.compute(cursor, user,
-                        line.invoice.currency, amount,
-                        line.invoice.company.currency, context=context)
-                res.append({
-                    'code': base_code_id,
-                    'amount': amount,
-                })
+        with Transaction().set_context(**context):
+            for tax in tax_obj.compute(tax_ids, price, line.quantity):
+                if line.invoice.type in ('out_invoice', 'in_invoice'):
+                    base_code_id = tax['tax'].invoice_base_code.id
+                    amount = tax['base'] * tax['tax'].invoice_base_sign
+                else:
+                    base_code_id = tax['tax'].credit_note_base_code.id
+                    amount = tax['base'] * tax['tax'].credit_note_base_sign
+                if base_code_id:
+                    amount = currency_obj.compute(
+                            line.invoice.currency, amount,
+                            line.invoice.company.currency)
+                    res.append({
+                        'code': base_code_id,
+                        'amount': amount,
+                    })
         return res
 
 InvoiceLine()
@@ -92,15 +88,12 @@ InvoiceLine()
 class Invoice(ModelSQL, ModelView):
     _name = 'account.invoice'
 
-    def _compute_taxes(self, cursor, user, invoice, context=None):
+    def _compute_taxes(self, invoice):
         tax_obj = self.pool.get('account.tax')
         currency_obj = self.pool.get('currency.currency')
-        if context is None:
-            context = {}
 
-        ctx = context.copy()
-        ctx.update(self.get_tax_context(cursor, user, invoice,
-            context=context))
+        context = {}
+        context.update(self.get_tax_context(invoice))
 
         res = {}
         for line in invoice.lines:
@@ -109,23 +102,20 @@ class Invoice(ModelSQL, ModelView):
                 continue
             price = line.unit_price - line.unit_price * line.discount / Decimal('100')
             tax_ids = [x.id for x in line.taxes]
-            for tax in tax_obj.compute(cursor, user, tax_ids, price,
-                    line.quantity, context=ctx):
-                key, val = self._compute_tax(cursor, user, tax, invoice.type,
-                        context=context)
-                val['invoice'] = invoice.id
-                if not key in res:
-                    res[key] = val
-                else:
-                    res[key]['base'] += val['base']
-                    res[key]['amount'] += val['amount']
+            with Transaction().set_context(**context):
+                for tax in tax_obj.compute(tax_ids, price, line.quantity):
+                    key, val = self._compute_tax(tax, invoice.type)
+                    val['invoice'] = invoice.id
+                    if not key in res:
+                        res[key] = val
+                    else:
+                        res[key]['base'] += val['base']
+                        res[key]['amount'] += val['amount']
         return res
 
-    def _on_change_lines_taxes(self, cursor, user, vals, context=None):
+    def _on_change_lines_taxes(self, vals):
         currency_obj = self.pool.get('currency.currency')
         tax_obj = self.pool.get('account.tax')
-        if context is None:
-            context = {}
         res = {
             'untaxed_amount': Decimal('0.0'),
             'tax_amount': Decimal('0.0'),
@@ -134,24 +124,19 @@ class Invoice(ModelSQL, ModelView):
         }
         currency = None
         if vals.get('currency'):
-            currency = currency_obj.browse(cursor, user, vals['currency'],
-                    context=context)
+            currency = currency_obj.browse(vals['currency'])
         computed_taxes = {}
         if vals.get('lines'):
-            ctx = context.copy()
-            ctx.update(self.get_tax_context(cursor, user, vals,
-                context=context))
             for line in vals['lines']:
                 if line.get('type', 'line') != 'line':
                     continue
                 res['untaxed_amount'] += line.get('amount', Decimal('0.0'))
 
                 price = line.get('unit_price') - line.get('unit_price') * line.get('discount') / Decimal('100')
-                for tax in tax_obj.compute(cursor, user, line.get('taxes', []),
-                        price,
-                        line.get('quantity', 0.0), context=context):
-                    key, val = self._compute_tax(cursor, user, tax,
-                            vals.get('type', 'out_invoice'), context=context)
+                for tax in tax_obj.compute(line.get('taxes', []), price,
+                        line.get('quantity', 0.0)):
+                    key, val = self._compute_tax(tax,
+                            vals.get('type', 'out_invoice'))
                     if not key in computed_taxes:
                         computed_taxes[key] = val
                     else:
@@ -171,7 +156,7 @@ class Invoice(ModelSQL, ModelView):
                 res['taxes']['remove'].append(tax.get('id'))
                 continue
             if currency:
-                if not currency_obj.is_zero(cursor, user, currency,
+                if not currency_obj.is_zero(currency,
                         computed_taxes[key]['base'] - \
                                 tax.get('base', Decimal('0.0'))):
                     res['tax_amount'] += computed_taxes[key]['amount']
@@ -201,13 +186,12 @@ class Invoice(ModelSQL, ModelView):
                 res['taxes'].setdefault('add', [])
                 res['taxes']['add'].append(computed_taxes[key])
         if currency:
-            res['untaxed_amount'] = currency_obj.round(cursor, user, currency,
+            res['untaxed_amount'] = currency_obj.round(currency,
                     res['untaxed_amount'])
-            res['tax_amount'] = currency_obj.round(cursor, user, currency,
-                    res['tax_amount'])
+            res['tax_amount'] = currency_obj.round(currency, res['tax_amount'])
         res['total_amount'] = res['untaxed_amount'] + res['tax_amount']
         if currency:
-            res['total_amount'] = currency_obj.round(cursor, user, currency,
+            res['total_amount'] = currency_obj.round(currency,
                     res['total_amount'])
         return res
 
