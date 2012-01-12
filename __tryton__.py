117b71c4b3eaa86dd0dbca9afba80ab84d43861d
Mathias Behrle <mbehrle@m9s.biz>
2012-01-12 14:49:06 +0100

    Increase version number

--- __tryton__.py
+++ __tryton__.py
@@ -3,7 +3,7 @@
 {
     'name': 'Account Invoice Discount',
     'name_de_DE': 'Fakturierung Rabatt',
-    'version': '2.1.1',
+    'version': '2.1.2',
     'author': 'virtual things',
     'email': 'info@virtual-things.biz',
     'website': 'http://www.virtual-things.biz/',
