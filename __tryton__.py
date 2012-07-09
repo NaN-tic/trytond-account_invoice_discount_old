9a2e6a2af10abb0bcae2ce0fdc08aafe1a6de679
resteve <resteve@zikzakmedia.com>
2012-07-09 10:24:51 +0200

    Add locale es_ES

--- __tryton__.py
+++ __tryton__.py
@@ -4,6 +4,7 @@
     'name': 'Account Invoice Discount',
     'name_ca_ES': 'Descomptes en factures',
     'name_de_DE': 'Fakturierung Rabatt',
+    'name_es_ES': 'Descuentos en facturas',
     'version': '2.1.3',
     'author': 'virtual things',
     'email': 'info@virtual-things.biz',
@@ -20,6 +21,10 @@
     - Ermöglicht die Eingabe von Rabatten pro Rechnungszeile
     - Fügt Rabattfeld im Bericht Rechnung hinzu
 ''',
+    'description_es_ES': '''Descuentos para ventas
+    - Define descuentos para líneas de ventas
+    - Añade campo descuento al tiquet de venta
+''',
     'depends': [
         'account_invoice'
         ],
@@ -28,6 +33,7 @@
         ],
     'translation': [
         'locale/ca_ES.po',
-        'locale/de_DE.po'
+        'locale/de_DE.po',
+        'locale/es_ES.po',
     ],
 }
