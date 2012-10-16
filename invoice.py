ec51c9ceec9bde5ce9d2fcbf974b4670ab26ddad
resteve <resteve@zikzakmedia.com>
2012-10-16 17:13:24 +0200

    Active Record

--- invoice.py
+++ invoice.py
@@ -4,205 +4,49 @@
 import copy
 from decimal import Decimal
 from trytond.model import ModelView, ModelSQL, fields
-from trytond.pyson import Not, Equal, Eval
+from trytond.pyson import If, Not, Equal, Eval
 from trytond.transaction import Transaction
-from trytond.pool import Pool
+from trytond.pool import Pool, PoolMeta
 
+__all__ = ['InvoiceLine']
+__metaclass__ = PoolMeta
 
-class InvoiceLine(ModelSQL, ModelView):
-    _name = 'account.invoice.line'
+class InvoiceLine:
+    'Invoice Line'
+    __name__ = 'account.invoice.line'
 
     discount = fields.Numeric('Discount %',
             digits=(16, Eval('currency_digits', 2)), states={
                 'invisible': Not(Equal(Eval('type'), 'line')),
-            }, depends=['currency_digits', 'type'])
-    def __init__(self):
-        super(InvoiceLine, self).__init__()
-        self.amount = copy.copy(self.amount)
-        if self.amount.on_change_with:
-            self.amount.on_change_with.extend(['discount'])
-        self._reset_columns()
+            }, on_change=['discount', 'product', 'quantity', 'type', 'unit_price'],
+            depends=['type','unit_price', 'quantity', 'amount'])
 
-    def default_discount(self):
+    @staticmethod
+    def default_discount():
         return 0.0
 
-    def on_change_with_amount(self, vals):
-        if vals.get('type') == 'line' and vals.get('discount') and \
-         vals.get('unit_price'):
-            vals = vals.copy()
-            vals['unit_price'] = (vals.get('unit_price') -
-                vals.get('unit_price') * vals.get('discount') * Decimal('0.01'))
-        return super(InvoiceLine, self).on_change_with_amount(vals)
-
-    def get_amount(self, ids, name):
-        currency_obj = Pool().get('currency.currency')
-        res = super(InvoiceLine, self).get_amount(ids, name)
-        for line in self.browse(ids):
-            if line.type == 'line':
-                currency = line.invoice and line.invoice.currency \
-                        or line.currency
-                res[line.id] = currency_obj.round(currency, \
-                        Decimal(str(line.quantity)) * line.unit_price - \
-                            (Decimal(str(line.quantity)) * line.unit_price *\
-                            (line.discount * Decimal('0.01'))))
-        return res
-
-    def _credit(self, line):
-        '''
-        Return values to credit line.
-        '''
-        res = super(InvoiceLine, self)._credit(line)
-        res['discount'] = line['discount']
-        return res
-
-    def _compute_taxes(self, line):
-        pool = Pool()
-        tax_obj = pool.get('account.tax')
-        currency_obj = pool.get('currency.currency')
-        invoice_obj = pool.get('account.invoice')
-
-        context = {}
-        context.update(invoice_obj.get_tax_context(line.invoice))
-        res = []
-        if line.type != 'line':
-            return res
-        price = line.unit_price - line.unit_price * line.discount / Decimal('100')
-        tax_ids = [x.id for x in line.taxes]
-        with Transaction().set_context(**context):
-            for tax in tax_obj.compute(tax_ids, price, line.quantity):
-                if line.invoice.type in ('out_invoice', 'in_invoice'):
-                    base_code_id = tax['tax'].invoice_base_code.id
-                    amount = tax['base'] * tax['tax'].invoice_base_sign
-                else:
-                    base_code_id = tax['tax'].credit_note_base_code.id
-                    amount = tax['base'] * tax['tax'].credit_note_base_sign
-                if base_code_id:
-                    with Transaction().set_context(
-                            date=line.invoice.currency_date):
-                        amount = currency_obj.compute(line.invoice.currency.id,
-                            amount, line.invoice.company.currency.id)
-                    res.append({
-                        'code': base_code_id,
-                        'amount': amount,
-                    })
-        return res
-
-InvoiceLine()
-
-
-class Invoice(ModelSQL, ModelView):
-    _name = 'account.invoice'
-
-    def _compute_taxes(self, invoice):
-        tax_obj = Pool().get('account.tax')
-        currency_obj = Pool().get('currency.currency')
-
-        context = {}
-        context.update(self.get_tax_context(invoice))
-
+    def on_change_discount(self):
         res = {}
-        for line in invoice.lines:
-            # Don't round on each line to handle rounding error
-            if line.type != 'line':
-                continue
-            price = line.unit_price - line.unit_price * line.discount / Decimal('100')
-            tax_ids = [x.id for x in line.taxes]
-            with Transaction().set_context(**context):
-                for tax in tax_obj.compute(tax_ids, price, line.quantity):
-                    key, val = self._compute_tax(tax, invoice.type)
-                    val['invoice'] = invoice.id
-                    if not key in res:
-                        res[key] = val
-                    else:
-                        res[key]['base'] += val['base']
-                        res[key]['amount'] += val['amount']
+        if self.discount == Decimal(0.0) and self.quantity != None:
+            res['amount'] = Decimal(self.quantity)*self.unit_price
+        if self.discount and self.unit_price and self.type == 'line':
+            res['amount'] = Decimal(self.quantity)*(self.unit_price -
+                self.unit_price * self.discount * Decimal('0.01'))
         return res
 
-    def _on_change_lines_taxes(self, vals):
-        currency_obj = Pool().get('currency.currency')
-        tax_obj = Pool().get('account.tax')
-        res = {
-            'untaxed_amount': Decimal('0.0'),
-            'tax_amount': Decimal('0.0'),
-            'total_amount': Decimal('0.0'),
-            'taxes': {},
-        }
-        currency = None
-        if vals.get('currency'):
-            currency = currency_obj.browse(vals['currency'])
-        computed_taxes = {}
-        if vals.get('lines'):
-            for line in vals['lines']:
-                if line.get('type', 'line') != 'line' or \
-                   line.get('unit_price') == None:
-                    continue
-                res['untaxed_amount'] += line.get('amount') or 0
-
-                discount = line.get('discount')
-                if isinstance(discount, float):
-                    discount = Decimal(str(line.get('discount')))
-                price = line.get('unit_price') - line.get('unit_price') \
-                    * discount / Decimal('100')
-                for tax in tax_obj.compute(line.get('taxes', []), price,
-                        line.get('quantity', 0.0)):
-                    key, val = self._compute_tax(tax,
-                            vals.get('type', 'out_invoice'))
-                    if not key in computed_taxes:
-                        computed_taxes[key] = val
-                    else:
-                        computed_taxes[key]['base'] += val['base']
-                        computed_taxes[key]['amount'] += val['amount']
-        tax_keys = []
-        for tax in vals.get('taxes', []):
-            if tax.get('manual', False):
-                res['tax_amount'] += tax.get('amount', Decimal('0.0'))
-                continue
-            key = (tax.get('base_code'), tax.get('base_sign'),
-                    tax.get('tax_code'), tax.get('tax_sign'),
-                    tax.get('account'), tax.get('description'))
-            tax_keys.append(key)
-            if key not in computed_taxes:
-                res['taxes'].setdefault('remove', [])
-                res['taxes']['remove'].append(tax.get('id'))
-                continue
-            if currency:
-                if not currency_obj.is_zero(currency,
-                        computed_taxes[key]['base'] - \
-                                tax.get('base', Decimal('0.0'))):
-                    res['tax_amount'] += computed_taxes[key]['amount']
-                    res['taxes'].setdefault('update', [])
-                    res['taxes']['update'].append({
-                        'id': tax.get('id'),
-                        'amount': computed_taxes[key]['amount'],
-                        'base': computed_taxes[key]['base'],
-                        })
-                else:
-                    res['tax_amount'] += tax.get('amount', Decimal('0.0'))
-            else:
-                if computed_taxes[key]['base'] - \
-                        tax.get('base', Decimal('0.0')) != Decimal('0.0'):
-                    res['tax_amount'] += computed_taxes[key]['amount']
-                    res['taxes'].setdefault('update', [])
-                    res['taxes']['update'].append({
-                        'id': tax.get('id'),
-                        'amount': computed_taxes[key]['amount'],
-                        'base': computed_taxes[key]['base'],
-                        })
-                else:
-                    res['tax_amount'] += tax.get('amount', Decimal('0.0'))
-        for key in computed_taxes:
-            if key not in tax_keys:
-                res['tax_amount'] += computed_taxes[key]['amount']
-                res['taxes'].setdefault('add', [])
-                res['taxes']['add'].append(computed_taxes[key])
-        if currency:
-            res['untaxed_amount'] = currency_obj.round(currency,
-                    res['untaxed_amount'])
-            res['tax_amount'] = currency_obj.round(currency, res['tax_amount'])
-        res['total_amount'] = res['untaxed_amount'] + res['tax_amount']
-        if currency:
-            res['total_amount'] = currency_obj.round(currency,
-                    res['total_amount'])
+    def on_change_product(self):
+        res = super(InvoiceLine, self).on_change_product()
+        res['discount'] = Decimal(0.0)
         return res
 
-Invoice()
+    def get_amount(self, name):
+        Currency = Pool().get('currency.currency')
+        res = super(InvoiceLine, self).get_amount(name)
+        if self.type == 'line':
+            currency = self.invoice and self.invoice.currency \
+                    or self.currency
+            res = Currency.round(currency, \
+                    Decimal(str(self.quantity)) * self.unit_price - \
+                        (Decimal(str(self.quantity)) * self.unit_price *\
+                        (self.discount * Decimal('0.01'))))
+        return res
--- setup.py
+++ setup.py
@@ -5,8 +5,15 @@
 
 from setuptools import setup
 import re
+import os
+import ConfigParser
 
-info = eval(open('__tryton__.py').read())
+config = ConfigParser.ConfigParser()
+config.readfp(open('tryton.cfg'))
+info = dict(config.items('tryton'))
+for key in ('depends', 'extras_depend', 'xml'):
+    if key in info:
+        info[key] = info[key].strip().splitlines()
 major_version, minor_version, _ = info.get('version', '0.0.1').split('.', 2)
 major_version = int(major_version)
 minor_version = int(minor_version)
@@ -22,10 +29,10 @@ requires.append('trytond >= %s.%s, < %s.%s' %
 
 setup(name='trytond_account_invoice_discount',
     version=info.get('version', '0.0.1'),
-    description=info.get('description', ''),
-    author=info.get('author', ''),
-    author_email=info.get('email', ''),
-    url=info.get('website', ''),
+    description='Tryton module to add discounts in lines invoice',
+    author='Zikzakmedia SL',
+    author_email='zikzak@zikzakmedia.com',
+    url='http://www.zikzakmedia.com',
     download_url="http://downloads.tryton.org/" + \
             info.get('version', '0.0.1').rsplit('.', 1)[0] + '/',
     package_dir={'trytond.modules.account_invoice_discount': '.'},
@@ -35,7 +42,7 @@ setup(name='trytond_account_invoice_discount',
     ],
     package_data={
         'trytond.modules.account_invoice_discount': info.get('xml', []) \
-                + info.get('translation', []),
+            + ['tryton.cfg', 'locale/*.po'],
     },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
